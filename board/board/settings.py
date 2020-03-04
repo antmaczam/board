4b9d1fb3bfbfa9bd35a169cbf511cab621599ae2
Antuan24 <antoniomaciaszambrano24@gmail.com>
2020-03-04 08:31:47 +0100

    Añadida configuración para Postgres

--- board/board/settings.py
+++ board/board/settings.py
@@ -75,8 +75,12 @@ WSGI_APPLICATION = 'board.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'boarddb',
+        'USER': 'board',
+        'PASSWORD': 'board',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
