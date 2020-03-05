f39f88821975becd2404e1f214721c2691ec040a
Antuan24 <antoniomaciaszambrano24@gmail.com>
2020-03-05 21:09:39 +0100

    Añadido el modelo Game para los juegos de mesa

--- board/rent/models.py
+++ board/rent/models.py
@@ -1,3 +1,39 @@
 from django.db import models
+from enum import Enum, auto
+from django.core.validators import URLValidator, MaxValueValidator, MinValueValidator
 
 # Create your models here.
+"""
+class Category(Enum):
+    cooperativo = auto()
+    terror = auto()
+    aventura = auto()
+"""
+
+class Status(Enum):
+    PE = 'Perfecto'
+    FA = 'Faltan piezas'
+    GA = 'Gastado'
+    IN = 'Injugable'
+
+#status = (('Pe','Perfecto'),('Fa','Faltan_piezas'),('Ga','Gastado'),('In','Injugable'))
+
+class Game(models.Model):
+    name = models.CharField(max_length=50, default='')
+    description = models.TextField(max_length=500, default='')
+    status = models.CharField(max_length=20,choices=[(str(x),x.value) for x in Status])
+    price = models.FloatField(default=0.0)
+    picture = models.CharField(max_length=500,validators=[URLValidator])
+    #rent
+    address = models.CharField(max_length=100, default='')
+    owner = models.CharField(max_length=50, default='admin')
+    #category
+    #reviews
+
+    @classmethod
+    def get_by_id(cls, cid):
+        return Game.objects.get(pk=cid)
+        
+
+    def __unicode__(self):
+        return '{}-{}'.format(self.get_name(),self.price)
