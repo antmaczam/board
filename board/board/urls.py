4bc4135e0a007a762897170bdd9499ee66c8247d
Antonio Macías Zambrano <36481170+antmaczam@users.noreply.github.com>
2020-03-06 18:33:18 +0100

    Realizada funcionalidad de listar juegos

--- board/board/urls.py
+++ board/board/urls.py
@@ -15,7 +15,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from rent.views import games_list
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('games',games_list),
 ]
--- board/rent/views.py
+++ board/rent/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from rent.models import Game
 
 # Create your views here.
+
+def games_list(request):
+    games = Game.objects.all()
+    return render(request,'games.html',{'games':games})
