272974cf082708567c017f253ce96b7d0309e0c5
pepefer97 <pepefernandez97@gmail.com>
2020-03-10 11:01:36 +0100

    FUNCIONALIDAD MOSTRAR UN JUEGO
    He añadido el controlador para mosrar un solo juego mediante su id en la url, adems de el template gamesDetail , que es un html muy simple y sencillo para visualizar el juego (como comprobacion de que funcionaba el metodo)

--- board/board/urls.py
+++ board/board/urls.py
@@ -14,10 +14,12 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path , re_path
 from rent.views import games_list
+from rent.views import games_detail
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('games',games_list),
+    re_path(r'gameDetail/(?P<id_game>\d+)',games_detail),
 ]
