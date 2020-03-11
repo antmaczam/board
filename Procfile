d62d6292d153e0999409e5cea81f834ccda00e20
juaparser <juaparser@alum.us.es>
2020-03-11 18:57:24 +0100

    [Fix] Trying deploy heroku

--- Procfile
+++ Procfile
@@ -1,6 +1,4 @@
 % prepara el repositorio para su despliegue. 
 release:sh -c 'cd board && python manage.py migrate'
 % especifica el comando para lanzar Decide
-web:gunicorn board.wsgi:application --log-file - --log-level debug
-python manage.py collectstatic --noinput
-manage.py migrate
+web: sh -c 'cd board && gunicorn board.wsgi --log-file -'
--- board/board/settings.py
+++ board/board/settings.py
@@ -129,10 +129,11 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
-STATIC_URL = '/board/staticfiles/'
-STATICFILES_DIR = [
-    os.path.join(BASE_DIR, 'staticfiles'),
-]
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
+STATIC_URL = '/static/'
+
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'staticfiles'),
+)
+
 django_heroku.settings(locals())
