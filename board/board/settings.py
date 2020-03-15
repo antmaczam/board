25037e3e55b6a04e6a336270d55825e398214bc9
juaparser <juaparser@alum.us.es>
2020-03-15 21:09:20 +0100

    [Fix] Fixing heroku postgres database

--- board/board/settings.py
+++ board/board/settings.py
@@ -86,8 +86,8 @@ WSGI_APPLICATION = 'board.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
 
-DATABASES['default'] = dj_database_url.config(conn_max_age=600, ssl_require=True)
 DATABASES = {}
+DATABASES['default'] = dj_database_url.config(conn_max_age=600, ssl_require=True)
 
 
 # Password validation
