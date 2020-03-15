881e82778f11086e76e6b8496eff994041c3c7c1
Gonzalo <gonferde@alum.us.es>
2020-03-15 19:23:44 +0100

    payments view

--- board/board/urls.py
+++ board/board/urls.py
@@ -16,6 +16,7 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from base import views
+from stripe import views as viewsStrip
 from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib.staticfiles.urls import staticfiles_urlpatterns
@@ -24,6 +25,8 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.index),
     path('base/', views.base),
+    path('stripe/aceptacion_pago', viewsStrip.aceptacion_pago),
+    path('stripe/pago_completado', viewsStrip.pago_completado),
 ]+ static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 
 urlpatterns += staticfiles_urlpatterns()
--- board/stripe/views.py
+++ board/stripe/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+def aceptacion_pago(request):
+    return render(request,'aceptacion_pago.html')
+
+def pago_completado(request):
+    return render(request,'pago_completado.html')    
