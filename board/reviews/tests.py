0aae7497db13ad172cff1efc863c9855810c8676
Gonzalo Aguilar <zalo.agui1@gmail.com>
2020-03-17 17:13:34 +0100

    [Test] Añadidos test unitarios de los módulos rent, reviews y user

--- board/reviews/tests.py
+++ board/reviews/tests.py
@@ -1,3 +1,50 @@
 from django.test import TestCase
 
-# Create your tests here.
+from user.models import User
+from reviews.models import Valoration, Comment
+
+class ValorationModelTestCase(TestCase):
+    
+    #Prepara una bbdd default con los objetos que se van a testear en este TestCase -------------------
+
+    def setUp(self):
+
+        self.user1 = User(username='prueba1',password='prueba123')
+        self.user1.save()
+
+        self.user2 = User(username='prueba2',password='prueba456')
+        self.user2.save()
+
+        self.valoration = Valoration(toUser=self.user1, fromUser=self.user2, rate=2.5)
+        self.valoration.save()
+
+        self.comment = Comment(toUser=self.user1, fromUser=self.user2, comment='Buen juego, mejor jugador')
+        self.comment.save()
+    
+    #Batería de test unitarios ------------------------------------------------------------------------
+
+    def test_get_valoration_toUser(self):
+        self.assertEquals(self.valoration.toUser, self.user1)
+    
+    def test_get_valoration_fromUser(self):
+        self.assertEquals(self.valoration.fromUser, self.user2)
+
+    def test_get_valoration_rate(self):
+        self.assertEquals(self.valoration.rate, 2.5)   
+
+    def test_get_comment_toUser(self):
+        self.assertEquals(self.comment.toUser, self.user1)
+
+    def test_get_comment_fromUser(self):
+        self.assertEquals(self.comment.fromUser, self.user2)
+
+    def test_get_comment_comment(self):
+        self.assertEquals(self.comment.comment, 'Buen juego, mejor jugador')             
+
+    #Borra los datos para terminar con los test ------------------------------------------------------
+    
+    def tearDown(self):
+        self.valoration.delete()
+        self.comment.delete()
+        self.user1.delete()
+        self.user2.delete()
