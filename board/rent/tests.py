26f5e8f6e4a509fcb6ffe5f4e9aae4e3c972d9d1
Antuan24 <antoniomaciaszambrano24@gmail.com>
2020-03-17 10:45:56 +0100

    [New] Añadido tests para el módulo 'rent'

--- board/rent/tests.py
+++ board/rent/tests.py
@@ -1,3 +1,30 @@
+import random
 from django.test import TestCase
 
-# Create your tests here.
+from user.models import User
+from rent.models import Game, Status
+
+class GameModelTestCase(TestCase):
+    
+    #Prepara una bbdd default con los objetos que se van a testear en este TestCase -------------------
+
+    def setUp(self):
+        self.user = User(username='prueba',password='prueba123')
+        self.user.save()
+        
+        self.game = Game(name='BANG',description='descripcion',status=Status.PE,price=2.5,picture='http://www.foto,com/foto,png',address='Los remedios',owner=self.user)
+        self.game.save()
+    
+    #Batería de test unitarios ------------------------------------------------------------------------
+
+    def test_get_owner(self):
+        self.assertEquals(self.game.owner,self.user)
+    
+    def test_get_name(self):
+        self.assertEquals(self.game.name,'BANG')
+
+    #Borra los datos para terminar con los test ------------------------------------------------------
+    
+    def tearDown(self):
+        self.game.delete()
+        self.user.delete()
