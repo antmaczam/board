d7fb9a01329bd6346828e25a5e0dae0e12972246
Antuan24 <antoniomaciaszambrano24@gmail.com>
2020-03-21 16:50:49 +0100

    [Fix] Cambiado el nombre del módulo

--- board/stripe/views.py
+++
@@ -1,8 +0,0 @@
-from django.shortcuts import render
-from stripe import views
-
-def pago_completado(request):
-    return render(request,'pago_completado.html')
-
-def aceptacion_pago(request):
-    return render(request,'aceptacion_pago.html') 
